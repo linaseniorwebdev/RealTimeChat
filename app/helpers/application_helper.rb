f686e0ae05207c5b8aae5c93a4f15548e9062bb1
linaseniorwebdev <lina.web.developer.senior@gmail.com>
2019-10-14 22:57:18 +0900

    Reset to original status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,19 @@
 module ApplicationHelper
-  def link_to_function(name, *args, &block)
-    html_options = args.extract_options!.symbolize_keys
+	def link_to_function(name, *args, &block)
+		html_options = args.extract_options!.symbolize_keys
 
-    function = block_given? ? update_page(&block) : args[0] || ''
-    onclick = "#{"#{html_options[:onclick]}; " if html_options[:onclick]}#{function}; return false;"
-    href = html_options[:href] || '#'
+		function = block_given? ? update_page(&block) : args[0] || ''
+		onclick = "#{"#{html_options[:onclick]}; " if html_options[:onclick]}#{function}; return false;"
+		href = html_options[:href] || '#'
 
-    content_tag(:a, name, html_options.merge(:href => href, :onclick => onclick, :class => "btn btn-primary"))
-  end
-  def link_to_add_fields(name, f, association)
-    #new_object = f.object.class.reflect_on_association(association).klass.new
-    new_object = f.object.send(association).build
-    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      render(association.to_s.singularize, :f => builder)
-    end
-    link_to_function(name, "add_fields(this, '#{association}', '#{escape_javascript(fields)}')")
-  end
+		content_tag(:a, name, html_options.merge(:href => href, :onclick => onclick))
+	end
+	def link_to_add_fields(name, f, association) 
+		#new_object = f.object.class.reflect_on_association(association).klass.new
+		new_object = f.object.send(association).build
+		fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+		  render(association.to_s.singularize, :f => builder)
+		end
+		link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+	end
 end
